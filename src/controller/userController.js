b6fe9f10ca77ba9a6f1d313a231898f816a397cc
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-16 06:16:41 +0700

    behasil login tanpa jwt

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 
 const index = (req, res) => {
   res.send('Dari conrolloersejdfjds')
@@ -22,11 +23,24 @@ const login_get = (req, res) => {
   res.send('login');
 };
 
-const login_post = (req, res) => {
+const login_post = async (req, res) => {
+  // console.log(req.body);
+  const { username, password } = req.body;
+
   try {
-
+    const user = await User.findOne({ username }) || await User.findOne({ email: username });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        res.status(200).json({ user: user.id })
+      } else {
+        throw Error('Password salah');
+      }
+    } else {
+      throw Error('Username atau email tidak ditemukan');
+    }
   } catch (err) {
-
+    res.status(400).json({ status: 'fail', message: err.message });
   };
 };
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,7 +9,7 @@ router.get('/signup', userController.signup_get);
 router.post('/signup', userController.signup_post);
 
 router.get('/login', userController.login_get);
-router.post('/login', () => {});
+router.post('/login', userController.login_post);
 
 router.get('/invite', () => {});
 router.post('/invite', () => {});
